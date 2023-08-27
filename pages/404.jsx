5944eb8fda4c4e705bf9c6be9e99cd27a12c230b
farhanyp <farhan.yudha2016we@gmail.com>
2023-08-27 10:45:11 +0700

    membuat halaman 404 costum

---
+++ pages/404.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Costum404() {
+  return (
+    <div>
+      <h1 className="title-not-found">Ooops....</h1>
+      <h1 className="title-not-found">Anda Tersesat</h1>
+    </div>
+  );
+}
+
+export default Costum404;
