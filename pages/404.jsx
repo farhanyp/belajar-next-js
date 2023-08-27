2a82a3fbbb67427c52c2033be5b8cbf4cb390bd2
farhanyp <farhan.yudha2016we@gmail.com>
2023-08-27 12:18:32 +0700

    learn auto redirect

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 function Costum404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000);
+  });
+
   return (
     <div>
       <h1 className="title-not-found">Ooops....</h1>
