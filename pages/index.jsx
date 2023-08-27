5888d12efc2335b85cb1ad8f457c17be6ebac122
farhanyp <farhan.yudha2016we@gmail.com>
2023-08-27 13:41:43 +0700

    menggunakan Image dari next

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Layout from '../components/Layout/index';
 
@@ -5,6 +6,8 @@ export default function Home() {
   return (
     <Layout pageTitle="Home Page">
       <h1 className={styles['title-homepage']}>Selamat Datang</h1>
+      <Image src="/motor.jpeg" width={200} height={200} />
+
     </Layout>
   );
 }
